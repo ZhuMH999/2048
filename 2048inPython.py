747f5edc65ba6575eda5866ca27a0f70ad819ba2
ryan <zhumh.ryan@gmail.com>
2025-01-11 22:04:04 +0800

    1/2 done, this commit is for a pull on my mac

--- 2048inPython.py
+++ 2048inPython.py
@@ -4,6 +4,8 @@ pygame.init()
 
 WIDTH, HEIGHT = 800, 600
 
+FONT = pygame.font.SysFont('Times New Roman', 30)
+
 class View:
     def __init__(self, model, win):
         self.model = model
@@ -11,12 +13,25 @@ class View:
 
     def draw(self):
         self.win.fill((100, 100, 100))
+        for i in range(self.model.y):
+            for j in range(self.model.x):
+                if self.model.board[i][j] != 0:
+                    self.get_text_widget_and_center((0, 0, 0), j*50+25, i*50+25, FONT, str(self.model.board[i][j]))
+
+    def get_text_widget_and_center(self, rgb, c_x, c_y, font, text):
+        widget = font.render(text, True, rgb)
+        rect = widget.get_rect()
+        rect.center = (c_x, c_y)
+        self.win.blit(widget, rect)
 
 class Model:
     def __init__(self):
         self.x = 4
         self.y = 4
         self.board = [[0] * self.x for _ in range(self.y)]
+        self.board[3][0] = 2
+        self.board[3][3] = 2
+        self.move_right()
 
     def move_left(self):
         for i in range(self.y):
@@ -37,6 +52,15 @@ class Model:
                         else:
                             done = True
 
+    def move_right(self):
+        for i in range(self.y):
+            for j in range(self.x-1):
+                done = False
+                while not done:
+                    if self.board[i][self.x-1-j] == 0 or self.board[i][self.x-j] == self.board[i][self.x-1-j]:
+                        if self.board[i][self.x-1-j] == 0:
+                            self.board[i][self.x-1-j]
+
 class Controller:
     def __init__(self):
         self.win = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -55,6 +79,9 @@ class Controller:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        self.model.move_left()
 
             self.view.draw()
 
