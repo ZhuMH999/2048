e81c29abd229a25d30f809b735498a0532b6346e
Ryan <zhumh.ryan@gmail.com>
2025-01-12 15:00:13 +0800

    GUI menu done its also almost the final commit

--- 2048inPython.py
+++ 2048inPython.py
@@ -5,8 +5,7 @@ pygame.init()
 
 WIDTH, HEIGHT = 800, 600
 
-
-TILE_SIZE = 60
+TILE_SIZE = 50
 TILE_GAP = 10
 TILE_COLORS = {0: (170, 170, 170),
                2: (237, 229, 218),
@@ -22,7 +21,8 @@ TILE_COLORS = {0: (170, 170, 170),
                2048: (200, 150, 30),
                4096: (10, 10, 10)}
 
-FONT = pygame.font.SysFont('Arial', 25)
+FONT = pygame.font.SysFont('Times New Roman', 25)
+MENU_FONT = pygame.font.SysFont('Times New Roman', 20)
 
 class View:
     def __init__(self, model, win):
@@ -31,19 +31,43 @@ class View:
 
     def draw(self):
         self.win.fill((100, 100, 100))
-        for i in range(self.model.y):
-            for j in range(self.model.x):
-                if self.model.board[i][j] > 2048:
-                    pygame.draw.rect(self.win, (187, 165, 61), (j*(TILE_SIZE+TILE_GAP)+10, i*(TILE_SIZE+TILE_GAP)+10, TILE_SIZE, TILE_SIZE))
-                    pygame.draw.rect(self.win, TILE_COLORS[4096], (j*(TILE_SIZE+TILE_GAP)+15, i*(TILE_SIZE+TILE_GAP)+15, TILE_SIZE-10, TILE_SIZE-10))
-                else:
-                    pygame.draw.rect(self.win, TILE_COLORS[self.model.board[i][j]], (j*(TILE_SIZE+TILE_GAP)+10, i*(TILE_SIZE+TILE_GAP)+10, TILE_SIZE, TILE_SIZE))
-
-                if self.model.board[i][j] != 0:
+        x, y = pygame.mouse.get_pos()
+        if self.model.x is not None:
+            for i in range(self.model.y):
+                for j in range(self.model.x):
                     if self.model.board[i][j] > 2048:
-                        self.get_text_widget_and_center((187, 165, 61), j * (TILE_SIZE + TILE_GAP) + 10 + TILE_SIZE/2, i * (TILE_SIZE + TILE_GAP) + 10 + TILE_SIZE/2, FONT, str(self.model.board[i][j]))
+                        pygame.draw.rect(self.win, (187, 165, 61), (j*(TILE_SIZE+TILE_GAP)+10, i*(TILE_SIZE+TILE_GAP)+10, TILE_SIZE, TILE_SIZE))
+                        pygame.draw.rect(self.win, TILE_COLORS[4096], (j*(TILE_SIZE+TILE_GAP)+15, i*(TILE_SIZE+TILE_GAP)+15, TILE_SIZE-10, TILE_SIZE-10))
                     else:
-                        self.get_text_widget_and_center((0, 0, 0), j * (TILE_SIZE + TILE_GAP) + 10 + TILE_SIZE/2, i * (TILE_SIZE + TILE_GAP) + 10 + TILE_SIZE/2, FONT, str(self.model.board[i][j]))
+                        pygame.draw.rect(self.win, TILE_COLORS[self.model.board[i][j]], (j*(TILE_SIZE+TILE_GAP)+10, i*(TILE_SIZE+TILE_GAP)+10, TILE_SIZE, TILE_SIZE))
+
+                    if self.model.board[i][j] != 0:
+                        if self.model.board[i][j] > 2048:
+                            self.get_text_widget_and_center((187, 165, 61), j * (TILE_SIZE + TILE_GAP) + 10 + TILE_SIZE/2, i * (TILE_SIZE + TILE_GAP) + 10 + TILE_SIZE/2, FONT, str(self.model.board[i][j]))
+                        else:
+                            self.get_text_widget_and_center((0, 0, 0), j * (TILE_SIZE + TILE_GAP) + 10 + TILE_SIZE/2, i * (TILE_SIZE + TILE_GAP) + 10 + TILE_SIZE/2, FONT, str(self.model.board[i][j]))
+
+            self.get_text_widget_and_center((0, 0, 0), self.model.x / 2 * (TILE_SIZE + TILE_GAP) + 10, self.model.y * (TILE_SIZE + TILE_GAP) + 10 + TILE_SIZE/2, FONT, 'Score: ' + str(self.model.score))
+            if self.model.x > 4:
+                self.get_text_widget_and_center((0, 0, 0), self.model.x * (TILE_SIZE + TILE_GAP) + 100, 10 + TILE_SIZE/2, MENU_FONT, 'Auto Move')
+
+                if self.model.x * (TILE_SIZE + TILE_GAP) + 30 < x < self.model.x * (TILE_SIZE + TILE_GAP) + 50 and TILE_SIZE/2 < y < TILE_SIZE/2 + 20:
+                    pygame.draw.rect(self.win, (120, 120, 120), (self.model.x * (TILE_SIZE + TILE_GAP) + 30, TILE_SIZE / 2, 20, 20))
+                else:
+                    if self.model.auto_move:
+                        pygame.draw.rect(self.win, (118, 150, 86), (self.model.x * (TILE_SIZE + TILE_GAP) + 30, TILE_SIZE/2, 20, 20))
+                    else:
+                        pygame.draw.rect(self.win, (70, 70, 70), (self.model.x * (TILE_SIZE + TILE_GAP) + 30, TILE_SIZE / 2, 20, 20))
+
+        else:
+            pygame.draw.rect(self.win, (80, 80, 80), (280, 175, 240, 250), border_radius=4)
+            pygame.draw.rect(self.win, (238, 238, 210), (285, 180, 230, 240), border_radius=4)
+            self.get_text_widget_and_center((118, 150, 86), 400, 200, MENU_FONT, 'Play a game?')
+            for i in range(3):
+                pygame.draw.rect(self.win, (100, 100, 100), (295, 220 + i * 60, 210, 50), border_radius=4)
+                if 295 < x < 505 and 220 + i * 60 < y < 270 + i * 60:
+                    pygame.draw.rect(self.win, (170, 170, 170), (300, 225 + i * 60, 200, 40), border_radius=4)
+                self.get_text_widget_and_center((238, 238, 210), 400, 245 + i * 60, MENU_FONT, str((i+1)*2+2) + 'x' + str((i+1)*2+2) + ' board')
 
     def get_text_widget_and_center(self, rgb, c_x, c_y, font, text):
         widget = font.render(text, True, rgb)
@@ -53,11 +77,12 @@ class View:
 
 class Model:
     def __init__(self):
-        self.x = 8
-        self.y = 8
-        self.board = [[0] * self.x for _ in range(self.y)]
+        self.x = None
+        self.y = None
+        self.board = []
+        self.score = 0
         self.end = False
-        self.spawn_new_num()
+        self.auto_move = False
 
     def move_left(self, check=False):
         has_moved = False
@@ -77,6 +102,7 @@ class Model:
                             elif self.board[i][j+temp-1] == self.board[i][j+temp]:
                                 if not check:
                                     self.board[i][j+temp-1] *= 2
+                                    self.score += self.board[i][j+temp-1]
                                 has_merged = True
                             if not check:
                                 self.board[i][j+temp] = 0
@@ -109,6 +135,7 @@ class Model:
                             elif self.board[i][j+temp] == self.board[i][j+temp-1]:
                                 if not check:
                                     self.board[i][j+temp] *= 2
+                                    self.score += self.board[i][j+temp]
                                 has_merged = True
                             if not check:
                                 self.board[i][j+temp-1] = 0
@@ -141,6 +168,7 @@ class Model:
                             elif self.board[i+temp-1][j] == self.board[i+temp][j]:
                                 if not check:
                                     self.board[i+temp-1][j] *= 2
+                                    self.score += self.board[i+temp-1][j]
                                 has_merged = True
                             if not check:
                                 self.board[i+temp][j] = 0
@@ -173,6 +201,7 @@ class Model:
                             elif self.board[i+temp][j] == self.board[i+temp-1][j]:
                                 if not check:
                                     self.board[i+temp][j] *= 2
+                                    self.score += self.board[i+temp][j]
                                 has_merged = True
                             if not check:
                                 self.board[i+temp-1][j] = 0
@@ -204,6 +233,29 @@ class Model:
         else:
             return False
 
+    def check_for_click_pos(self, x, y):
+        if self.x is None:
+            done = False
+            if 295 < x < 505:
+                if 220 < y < 270:
+                    self.x = self.y = 4
+                    done = True
+                elif 280 < y < 330:
+                    self.x = self.y = 6
+                    done = True
+                elif 340 < y < 390:
+                    self.x = self.y = 8
+                    done = True
+            if done:
+                self.board = [[0] * self.x for _ in range(self.y)]
+                self.spawn_new_num()
+        else:
+            if self.x * (TILE_SIZE + TILE_GAP) + 30 < x < self.x * (TILE_SIZE + TILE_GAP) + 50 and TILE_SIZE / 2 < y < TILE_SIZE / 2 + 20:
+                if self.auto_move:
+                    self.auto_move = False
+                else:
+                    self.auto_move = True
+
 class Controller:
     def __init__(self):
         self.win = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -219,15 +271,16 @@ class Controller:
         while run:
             clock.tick(60)
 
-            self.model.move_up()
-            self.model.move_right()
-            self.model.move_down()
-            self.model.move_right()
+            if self.model.auto_move:
+                self.model.move_right()
+                self.model.move_up()
+                self.model.move_right()
+                self.model.move_down()
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
-                elif event.type == pygame.KEYDOWN:
+                elif event.type == pygame.KEYDOWN and self.model.x is not None:
                     if event.key == pygame.K_LEFT:
                         self.model.move_left()
                     elif event.key == pygame.K_RIGHT:
@@ -236,6 +289,10 @@ class Controller:
                         self.model.move_up()
                     elif event.key == pygame.K_DOWN:
                         self.model.move_down()
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        x, y = pygame.mouse.get_pos()
+                        self.model.check_for_click_pos(x, y)
 
             self.view.draw()
 
